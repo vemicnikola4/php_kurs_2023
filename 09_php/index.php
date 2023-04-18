80387bfc57af6741cc360e4cc3dc38e5304c3380
vemicnikola4 <vemicnikola4@gmail.com>
2023-04-18 16:26:12 +0200

    added vezbanje 07 08 09 10 in various folders

--- 09_php/index.php
+++ 09_php/index.php
@@ -1,5 +1,32 @@
 <?php
+    $a = "Zdravo svete!";
+    echo $a;
+    echo "<br>";
+    $a = 1;
+    echo "Vrednost a je $a";
+    // echo 'Vrednost a je $a';ovo ne bi radilo jer je 1navodnik Da bi unutar stringa prikazao int mora da ima duple navodnike.
+    $c ="3";
+    echo "<br>";
+    echo $c * 2;
     
-    echo "Tu sam";
+    $h =20;
+    $m = 35;
+    $rez = $h * 60 + $m;
+    echo "<hr>";
 
+    echo "Od ponoci je proslo ".$rez." minuta.";
+    $sati = date('G');//uzima vreme sa lokalnog servera ili udaljenog servera. Ako server nije dobro podesendacepogresno vreme;
+    $minuti = date('i');
+    echo "<hr>";
+    echo "<p>Tacno je ".$sati . "sati i ".$minuti . " minuta . </p>";
+
+    date_default_timezone_set('Europe/Belgrade');//seteri i geteri
+      
+    $h =date('G');
+    $m = date('i');
+   
+    echo "<hr>";
+    $rez = $h * 60 + $m;
+    echo "Od ponoci je proslo ".number_format($rez,0,",",".")." minuta.";
+    echo "<hr>";
 ?>
